ce51ddd44201f62c51ca05869cee3d91760267f4
朱淑献 <zhushuxian@digitalgd.com.cn>
2022-09-23 14:58:12 +0800

    [!]初始化源码1、https://www.bilibili.com/read/cv16437257/   2、https://blog.csdn.net/hell_oword/article/details/111757888

---
+++ spring-boot-tests/spring-boot-demo/src/main/java/com/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description TODO
+ * @Date 2022/9/23 14:45
+ * @Created by zhushuxian
+ */
+@RestController
+@EnableAutoConfiguration
+@SpringBootConfiguration
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+
+}
