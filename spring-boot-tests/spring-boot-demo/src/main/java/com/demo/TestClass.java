1bf283fa418158095cfbe60833ba98de1c471006
朱淑献 <zhushuxian@digitalgd.com.cn>
2022-10-20 16:58:51 +0800

    [!]ConfigurableApplicationContext run流程注释

---
+++ spring-boot-tests/spring-boot-demo/src/main/java/com/demo/TestClass.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @Description TODO
+ * @Date 2022/10/18 10:48
+ * @Created by zhushuxian
+ */
+public class TestClass {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
